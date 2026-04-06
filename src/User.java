f7e409d116414ecc47429b5825da180f72136eee
Hazik-Allaie <allaiehaziq786@gmail.com>
2026-04-06 22:45:09 +0800

    classes added

--- src/User.java
+++ src/User.java
@@ -1,12 +1,78 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
-/**
- *
- * @author HAZIK
- */
 public class User {
+    private String userid;
+    private String name;
+    private int age ;
+    private String email ;
+    private String username ;
+    private String password ;
+    private String contact ;
+    private String role;
+
+    
+    //Constructor
+    
+public  User(String userid,String name,int age,String email ,String username ,String password,String contact ,String role){
+this.name=name;
+this.username=username;
+this.userid=userid;
+this.age=age;
+this.email=email;
+this.password=password;
+this.contact=contact;
+this.role=role;}
+
+
+    //getter 
+    
+    public String getName(){
+    return name ;}
+    
+    public String getUserid(){
+    return userid;}
+    
+    public int getAge(){
+    return age ;}
+    
+    public String getEmail(){
+    return email ; }
+    
+     public String getUsername(){
+    return username ; }
+     
+    public String getPassword(){
+    return password ; }
+    
+    public String getContact(){
+    return contact ; }
+    
+    public String  getRole(){
+    return role ; }
+    
+    
+    //setter 
+    public void setUserid( String userid ){
+    this.userid =userid;}
+    
+    public void setName( String name ){
+    this.name = name ;}
+    
+    public void setAge( int age ){
+    this.age = age ;}
+    
+    public void setEmail (String email){
+    this.email = email ;}
+    
+    public void setUsername( String username ){
+    this.username =username;}
+    
+    public void setPassword (String password){
+    this.password=password;}
+    
+    public void setContact(String contact){
+    this.contact=contact;}
+    
+    public void setRole(String role){
+    this.role=role;}
     
 }
